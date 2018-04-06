f693c0e64d63718988e100e9823a013f7ae08b66
Artyom <admin@VLAN-25-58-055.rd.mirea.ru>
2018-04-06 21:03:48 +0300

    Server.js

---
+++ Server.js
@@ -0,0 +1,20 @@
+﻿var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+  var html = `<html><head>omegalul</head>
+<body>
+
+  <p>Начало...</p>
+  <script>
+    alert( 'Самый русский текст' );
+  </script>
+
+  <p>...Конец</p>
+
+</body>
+
+</html>`
+  res.end(html);
+  console.log("I have done that")
+}).listen(8080)
